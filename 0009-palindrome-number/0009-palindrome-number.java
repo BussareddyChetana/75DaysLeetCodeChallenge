7fafee85467243b67ff2d54386e1d403054f3110
BussareddyChetana <bussareddychetana@gmail.com>
2026-06-09 21:32:51 +0530

    Time: 5 ms (78.6%), Space: 45.8 MB (71.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-      int rn=0;
       int dup=x;
+      int rev=0;
       while(x>0){
         int ld=x%10;
-        rn=(rn*10)+ld;
+        rev=(rev*10)+ld;
         x=x/10;
       }
-      if(dup==rn){
+      if(rev==dup){
         return true;
-      }else{
+      }
+      else{
         return false;
-      }  
+
+      }
     }
 }
